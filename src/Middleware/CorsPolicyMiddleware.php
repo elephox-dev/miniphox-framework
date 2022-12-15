6dfa77591e40be69e002da4b3f525c8756cbfd45
Ricardo Boss <contact@ricardoboss.de>
2022-12-15 22:14:46 +0100

    Use fruitcake/php-cors fork

--- src/Middleware/CorsPolicyMiddleware.php
+++ src/Middleware/CorsPolicyMiddleware.php
@@ -3,44 +3,25 @@ declare(strict_types=1);
 
 namespace Elephox\Miniphox\Middleware;
 
+use Fruitcake\Cors\CorsService;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 class CorsPolicyMiddleware
 {
     public function __construct(
-        private readonly CorsOptions $options,
+        private readonly CorsService $cors,
     )
     {
     }
 
     public function __invoke(ServerRequestInterface $request, callable $next): ResponseInterface
     {
-        if ($this->matches($request)) {
-            return $this->handle($request);
+        // TODO: check if request path should be handled
+        if (!$this->cors->isPreflightRequest($request)) {
+            return $next($request);
         }
 
-        return $next($request);
-    }
-
-    private function matches(ServerRequestInterface $request): bool
-    {
-        if (!$this->isCorsRequest($request)) {
-            return false;
-        }
-
-        // TODO: check path, requested method, requested headers, origin
-
-        return true;
-    }
-
-    private function isCorsRequest(ServerRequestInterface $request): bool
-    {
-        return strtolower($request->getMethod()) === 'options';
-    }
-
-    private function handle(ServerRequestInterface $request): ResponseInterface
-    {
-        // TODO: handle CORS request
+        return $this->cors->handlePreflightRequest($request);
     }
 }
